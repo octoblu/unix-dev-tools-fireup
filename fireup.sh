9a3057997b671791ce73c13267cd8a04ef12ada9
Peter DeMartini <thepeterdemartini@gmail.com>
2016-07-29 10:30:36 -0700

    v1.1.1 fixed template problem

--- fireup.sh
+++ fireup.sh
@@ -59,6 +59,36 @@ check_git(){
   fi
 }
 
+init_repo() {
+  local project_dir="$1"
+  local repo_name="$2"
+  cat > "$project_dir/LICENSE" <<- EOM
+  The MIT License (MIT)
+
+  Copyright (c) 2016 Octoblu
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  EOM
+
+  echo "# ${repo_name}" > "$project_dir/README.md"
+}
+
 create_repo() {
   local create="$1"
   local github_owner="$2"
@@ -69,11 +99,10 @@ create_repo() {
     return 1
   fi
   echo '* creating repo'
-  local template_dir="$(script_directory)/project-template"
   mkdir -p "$project_dir" && \
     cd "$project_dir" && \
     git init && \
-    cp $template_dir/* "$project_dir" && \
+    init_repo "$project_dir" "$repo_name" && \
     hub create "$github_owner/$repo_name" && \
     git add . && \
     git commit -m "initial commit" && \
